5052c0440c32ad40b52958e4f8bfd818d0553c55
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-12-17 12:41:55 -0300

    site terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,26 @@ const listaMenu = document.querySelector('.navegacao__listaLinks');
 
 const hamburguer = document.querySelector('.hamburguer');
 
-function menuShow() {
+function menuShow(event) {
 
     listaMenu.classList.toggle('ativado');
 
     hamburguer.classList.toggle('ativado');
 
+    // ACESSIBILIDADE
+    if (event.type == 'touchstart') event.preventDefault();
+
+    const ativado = listaMenu.classList.contains('ativado');
+
+    event.currentTarget.setAttribute('aria-expanded', ativado);
+
+    if (ativado) {
+        event.currentTarget.setAttribute('aria-label', 'Botão fechar menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Botão abrir menu');
+    }
+
 }
 
 btnMenu.addEventListener('click', menuShow);
+btnMenu.addEventListener('touchstart', menuShow);
